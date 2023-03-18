095e32abdb77761af91c778b2c379e224c08ac60
chandukotari <chandukotari.1998@gmail.com>
2023-03-17 19:09:08 -0500

    Assignment-3 commit-2
    Added Carousel successfully. Also implemented button action using java script.

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,4 +1,36 @@
 $(document).ready(function() {
+  $("#mycarousel").carousel({ interval: 2000 });
+  $("#carouselButton").click(function () {
+        if ($("#carouselButton").children('span').hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children('span').removeClass('fa-pause');
+            $("#carouselButton").children('span').addClass('fa-play');
+        }
+        else if ($("#carouselButton").children('span').hasClass('fa-play')) {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children('span').removeClass('fa-play');
+            $("#carouselButton").children('span').addClass('fa-pause');
+        }
+    });
+
+
+      $(".item1").click(function () {
+          $("#mycarousel").carousel(0);
+      });
+      $(".item2").click(function () {
+          $("#mycarousel").carousel(1);
+      });
+      $(".item3").click(function () {
+          $("#mycarousel").carousel(2);
+      });
+
+      // Enable Carousel Controls
+      $("#prev_button").click(function () {
+          $("#mycarousel").carousel("prev");
+      });
+      $("#next_button").click(function () {
+          $("#mycarousel").carousel("next");
+      });
 
   $("#Login_btn").click(function() {
   $("#LoginModal").modal();
